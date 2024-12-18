10e94fdee1b6665daa23abf8c2929f8d77af4e3a
Starrywiki <starrywiki@gmail.com>
2024-12-18 22:05:28 +0800

    子任务一

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -1,79 +1,175 @@
-#include "Def.hpp"
-#include "value.hpp"
-#include "expr.hpp"
-#include "RE.hpp"
-#include "syntax.hpp"
 #include <cstring>
-#include <vector>
 #include <map>
+#include <vector>
 
-extern std :: map<std :: string, ExprType> primitives;
-extern std :: map<std :: string, ExprType> reserved_words;
+#include "Def.hpp"
+#include "RE.hpp"
+#include "expr.hpp"
+#include "syntax.hpp"
+#include "value.hpp"
 
-Value Let::eval(Assoc &env) {} // let expression
+extern std ::map<std ::string, ExprType> primitives;
+extern std ::map<std ::string, ExprType> reserved_words;
 
-Value Lambda::eval(Assoc &env) {} // lambda expression
+Value Let::eval(Assoc &env) {}  // let expression
 
-Value Apply::eval(Assoc &e) {} // for function calling
+Value Lambda::eval(Assoc &env) {}  // lambda expression
 
-Value Letrec::eval(Assoc &env) {} // letrec expression
+Value Apply::eval(Assoc &e) {}  // for function calling
 
-Value Var::eval(Assoc &e) {} // evaluation of variable
+Value Letrec::eval(Assoc &env) {}  // letrec expression
 
-Value Fixnum::eval(Assoc &e) {} // evaluation of a fixnum
+Value Var::eval(Assoc &e) {}  // evaluation of variable
 
-Value If::eval(Assoc &e) {} // if expression
+Value Fixnum::eval(Assoc &e) { return IntegerV(n); }  // evaluation of a fixnum
 
-Value True::eval(Assoc &e) {} // evaluation of #t
+Value If::eval(Assoc &e) {}  // if expression
 
-Value False::eval(Assoc &e) {} // evaluation of #f
+Value True::eval(Assoc &e) { return BooleanV(true); }  // evaluation of #t
 
-Value Begin::eval(Assoc &e) {} // begin expression
+Value False::eval(Assoc &e) { return BooleanV(false); }  // evaluation of #f
 
-Value Quote::eval(Assoc &e) {} // quote expression
+Value Begin::eval(Assoc &e) {}  // begin expression
 
-Value MakeVoid::eval(Assoc &e) {} // (void)
+Value Quote::eval(Assoc &e) {
+    auto istrue = dynamic_cast<TrueSyntax *>(s.get());
+    auto isfalse = dynamic_cast<TrueSyntax *>(s.get());
+    auto isnum = dynamic_cast<Number *>(s.get());
+    auto ispair = dynamic_cast<Pair *>(s.get());
+    auto issymbol = dynamic_cast<Identifier *>(s.get());
+    auto issymbols = dynamic_cast<List *>(s.get());
+    if (istrue) {
+        return BooleanV(true);
+    } else if (isfalse) {
+        return BooleanV(true);
+    } else if (isnum) {
+        return IntegerV(isnum->n);
+    } else if (ispair) {
+        return PairV(ispair->car, ispair->cdr);
+    } else if (issymbol) {
+        return SymbolV(issymbol->s);
+    } else if (issymbols) {
+        int len = issymbols->stxs.size();
+        if (len == 0) {
+            return NullV();
+        } else if (len == 3 &&
+                   dynamic_cast<Identifier *>(issymbols->stxs[1].get())->s ==
+                       ".") {
+            return PairV((Expr(new Quote(issymbols->stxs[0]))).get()->eval(e),
+                         (Expr(new Quote(issymbols->stxs[2]))).get()->eval(e));
+        } else {
+            Value res = NullV();
+            for (int i = len - 1; i >= 0; --i) {
+                res = PairV(
+                    (Expr(new Quote(issymbols->stxs[i]))).get()->eval(e), res);
+            }
+            return res;
+        }
+    } else {
+        return NullV();
+    }
+}  // quote expression
 
-Value Exit::eval(Assoc &e) {} // (exit)
+Value MakeVoid::eval(Assoc &e) { return VoidV(); }  // (void)
 
-Value Binary::eval(Assoc &e) {} // evaluation of two-operators primitive
+Value Exit::eval(Assoc &e) { return TerminateV(); }  // (exit)
 
-Value Unary::eval(Assoc &e) {} // evaluation of single-operator primitive
+Value Binary::eval(Assoc &e) {
+    return evalRator(rand1.get()->eval(e), rand2.get()->eval(e));
+}  // evaluation of two-operators primitive
 
-Value Mult::evalRator(const Value &rand1, const Value &rand2) {} // *
+Value Unary::eval(Assoc &e) {}  // evaluation of single-operator primitive
 
-Value Plus::evalRator(const Value &rand1, const Value &rand2) {} // +
+Value Mult::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    return IntegerV((dynamic_cast<Integer *>(rand1.get())->n) *
+                    dynamic_cast<Integer *>(rand2.get())->n);
+}  // *
 
-Value Minus::evalRator(const Value &rand1, const Value &rand2) {} // -
+Value Plus::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    return IntegerV((dynamic_cast<Integer *>(rand1.get())->n) +
+                    dynamic_cast<Integer *>(rand2.get())->n);
+}  // +
 
-Value Less::evalRator(const Value &rand1, const Value &rand2) {} // <
+Value Minus::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    return IntegerV((dynamic_cast<Integer *>(rand1.get())->n) -
+                    dynamic_cast<Integer *>(rand2.get())->n);
+}  // -
 
-Value LessEq::evalRator(const Value &rand1, const Value &rand2) {} // <=
+Value Less::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    int num1 = dynamic_cast<Integer *>(rand1.get())->n;
+    int num2 = dynamic_cast<Integer *>(rand2.get())->n;
+    if (num1 < num2) return BooleanV(true);
+    return BooleanV(false);
+}  // <
 
-Value Equal::evalRator(const Value &rand1, const Value &rand2) {} // =
+Value LessEq::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    int num1 = dynamic_cast<Integer *>(rand1.get())->n;
+    int num2 = dynamic_cast<Integer *>(rand2.get())->n;
+    if (num1 <= num2) return BooleanV(true);
+    return BooleanV(false);
+}  // <=
 
-Value GreaterEq::evalRator(const Value &rand1, const Value &rand2) {} // >=
+Value Equal::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    int num1 = dynamic_cast<Integer *>(rand1.get())->n;
+    int num2 = dynamic_cast<Integer *>(rand2.get())->n;
+    if (num1 == num2) return BooleanV(true);
+    return BooleanV(false);
+}  // =
 
-Value Greater::evalRator(const Value &rand1, const Value &rand2) {} // >
+Value GreaterEq::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    int num1 = dynamic_cast<Integer *>(rand1.get())->n;
+    int num2 = dynamic_cast<Integer *>(rand2.get())->n;
+    if (num1 >= num2) return BooleanV(true);
+    return BooleanV(false);
+}  // >=
 
-Value IsEq::evalRator(const Value &rand1, const Value &rand2) {} // eq?
+Value Greater::evalRator(const Value &rand1, const Value &rand2) {
+    if (rand1->v_type != V_INT || rand2->v_type != V_INT)
+        throw RuntimeError("戳啦");
+    int num1 = dynamic_cast<Integer *>(rand1.get())->n;
+    int num2 = dynamic_cast<Integer *>(rand2.get())->n;
+    if (num1 > num2) return BooleanV(true);
+    return BooleanV(false);
+}  // >
 
-Value Cons::evalRator(const Value &rand1, const Value &rand2) {} // cons
+Value IsEq::evalRator(const Value &rand1, const Value &rand2) {}  // eq?
 
-Value IsBoolean::evalRator(const Value &rand) {} // boolean?
+Value Cons::evalRator(const Value &rand1, const Value &rand2) {
+    return PairV(rand1, rand2);
+}  // cons
 
-Value IsFixnum::evalRator(const Value &rand) {} // fixnum?
+Value IsBoolean::evalRator(const Value &rand) {}  // boolean?
 
-Value IsSymbol::evalRator(const Value &rand) {} // symbol?
+Value IsFixnum::evalRator(const Value &rand) {}  // fixnum?
 
-Value IsNull::evalRator(const Value &rand) {} // null?
+Value IsSymbol::evalRator(const Value &rand) {}  // symbol?
 
-Value IsPair::evalRator(const Value &rand) {} // pair?
+Value IsNull::evalRator(const Value &rand) {}  // null?
 
-Value IsProcedure::evalRator(const Value &rand) {} // procedure?
+Value IsPair::evalRator(const Value &rand) {}  // pair?
 
-Value Not::evalRator(const Value &rand) {} // not
+Value IsProcedure::evalRator(const Value &rand) {}  // procedure?
 
-Value Car::evalRator(const Value &rand) {} // car
+Value Not::evalRator(const Value &rand) {}  // not
 
-Value Cdr::evalRator(const Value &rand) {} // cdr
+Value Car::evalRator(const Value &rand) {
+    return Value(dynamic_cast<Pair *>(rand.get())->car);
+}  // car
+
+Value Cdr::evalRator(const Value &rand) {
+    return Value(dynamic_cast<Pair *>(rand.get())->cdr);
+}  // cdr
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,33 +1,155 @@
-#ifndef PARSER 
+#ifndef PARSER
 #define PARSER
 
-// parser of myscheme 
+// parser of myscheme
 
-#include "RE.hpp"
-#include "Def.hpp"
-#include "syntax.hpp"
-#include "expr.hpp"
-#include <map>
 #include <cstring>
 #include <iostream>
+#include <map>
+
+#include "Def.hpp"
+#include "RE.hpp"
+#include "expr.hpp"
+#include "syntax.hpp"
 #define mp make_pair
-using std :: string;
-using std :: vector;
-using std :: pair;
+using std ::pair;
+using std ::string;
+using std ::vector;
 
-extern std :: map<std :: string, ExprType> primitives;
-extern std :: map<std :: string, ExprType> reserved_words;
+extern std ::map<std ::string, ExprType> primitives;
+extern std ::map<std ::string, ExprType> reserved_words;
 
-Expr Syntax :: parse(Assoc &env) {}
+Expr Syntax ::parse(Assoc &env) {}
 
-Expr Number :: parse(Assoc &env) {}
+Expr Number ::parse(Assoc &env) { return Expr(new Fixnum(n)); }
 
-Expr Identifier :: parse(Assoc &env) {}
+Expr Identifier ::parse(Assoc &env) { return Expr(new Var(s)); }
 
-Expr TrueSyntax :: parse(Assoc &env) {}
+Expr TrueSyntax ::parse(Assoc &env) { return Expr(new True()); }
 
-Expr FalseSyntax :: parse(Assoc &env) {}
+Expr FalseSyntax ::parse(Assoc &env) { return Expr(new False()); }
 
-Expr List :: parse(Assoc &env) {}
+Expr List ::parse(Assoc &env) {
+    if (stxs.empty()) {
+        return Expr(new MakeVoid());
+    }
+    auto op = dynamic_cast<Identifier *>(stxs[0].get())->s;
+    if (primitives.find(op) != primitives.end()) {
+        switch (primitives[op]) {
+            case E_MINUS:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Minus(stxs[1]->parse(env),
+                                          stxs[2]->parse(env)));
+                }
+                break;
+            case E_PLUS:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Plus(stxs[1]->parse(env),
+                                         stxs[2]->parse(env)));
+                }
+                break;
+            case E_MUL:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Mult(stxs[1]->parse(env),
+                                         stxs[2]->parse(env)));
+                }
+                break;
+            case E_GT:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Greater(stxs[1]->parse(env),
+                                            stxs[2]->parse(env)));
+                }
+                break;
+            case E_GE:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new GreaterEq(stxs[1]->parse(env),
+                                              stxs[2]->parse(env)));
+                }
+                break;
+            case E_LT:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Less(stxs[1]->parse(env),
+                                         stxs[2]->parse(env)));
+                }
+                break;
+            case E_LE:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new LessEq(stxs[1]->parse(env),
+                                           stxs[2]->parse(env)));
+                }
+                break;
+            case E_EQ:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Equal(stxs[1]->parse(env),
+                                          stxs[2]->parse(env)));
+                }
+                break;
+            case E_VOID:
+                if (stxs.size() != 1) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new MakeVoid());
+                }
+                break;
+            case E_EXIT:
+                if (stxs.size() != 1) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Exit());
+                }
+                break;
+            case E_CONS:
+                if (stxs.size() != 3) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Cons(stxs[1].parse(env),
+                                         stxs[2].parse(env)));
+                }
+            case E_CAR:
+                if (stxs.size() != 2) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Car(stxs[1].parse(env)));
+                }
+            case E_CDR:
+                if (stxs.size() != 2) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Cdr(stxs[1].parse(env)));
+                }
 
+            default:
+                break;
+        }
+    } else if (reserved_words.find(op) != reserved_words.end()) {
+        switch (reserved_words[op]) {
+            case E_QUOTE:
+                if (stxs.size() != 2) {
+                    throw RuntimeError("错啦");
+                } else {
+                    return Expr(new Quote(stxs[1]));
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
 #endif
